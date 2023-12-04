74861c02347868904f024043f8e7579b0815a8ac
P.HungTham <hunggtham@gmail.com>
2023-12-04 11:05:49 +0900

    added background-image

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,13 @@ import Header from "./component/Header";
 import AddNewList from "./component/AddNewList";
 import styled from "styled-components";
 
-const AppContainer = styled.div``;
+const AppContainer = styled.div`
+  background-image: url("/background.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 98vh;
+`;
 
 function App() {
   return (
